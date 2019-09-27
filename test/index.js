cd3dafa76c5cf260d291809db7baea6b0325b186
Caio Gondim <me@caiogondim.com>
2019-09-27 10:44:18 -0400

    Add tests

--- test/index.js
+++ test/index.js
@@ -38,3 +38,12 @@ test(`invalid query`, () => {
   expect(safeChain(obj, '[a].b*&%$%ˆ')).toBe(undefined)
   expect(safeChain(obj, '[a].b[c]]')).toBe(undefined)
 })
+
+test('non-objects input', () => {
+  expect(safeChain(undefined, 'a.b.c')).toBe(undefined)
+  expect(safeChain(null, 'a.b.c')).toBe(undefined)
+  expect(safeChain(false, 'a.b.c')).toBe(undefined)
+  expect(safeChain(true, 'a.b.c')).toBe(undefined)
+  expect(safeChain(2, 'a.b.c')).toBe(undefined)
+  expect(safeChain('lorem ipsum', 'a.b.c')).toBe(undefined)
+})
